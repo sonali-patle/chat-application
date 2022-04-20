31c36e8f4e78a758cbe18d08a50e2f5458a70792
sonali-patle <sonalipatle8@gmail.com>
2022-04-20 19:47:05 +0530

    chat application

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,22 @@
+class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+def current_user
+  if !@current_user.present? && session[:user_id]
+    @current_user = User.find(session[:user_id])
+  end
+  @current_user
+end
+
+def logged_in?
+  current_user.present? ? true: false
+end
+
+def require_user
+  if !logged_in?
+      flash[:error] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+end
+
+end
